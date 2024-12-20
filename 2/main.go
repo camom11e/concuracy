f6f8aee254d0799a01326fa3f6cd5fc3877892f6
camom11e <49825585+camom11e@users.noreply.github.com>
2024-12-20 19:54:23 +0300

    4

--- 2/main.go
+++ 2/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
+
 func main() {
+	numChan := make(chan int)
+	strChan := make(chan string)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for num := range numChan {
+				strChan <- strconv.Itoa(num)
+			}
+		}()
+	}
+	go func() {
+		for i := 1; i <= 10; i++ {
+			numChan <- i
+		}
+		close(numChan)
+	}()
+
+	go func() {
+		for str := range strChan {
+			fmt.Println(str)
+		}
+	}()
+
+	wg.Wait()
+	close(strChan)
 
 }
